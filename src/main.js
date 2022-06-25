66cfd1ab453338879e134666b92bee9772d88cb6
Aurimas <vajkis1@gmail.com>
2022-06-25 18:48:08 +0300

    data

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
-const rows = 20
-const columns = 30
+// render main window
+
+const rows = 21
+const columns = 31
 
 const mainEl = document.querySelector('main')
 mainEl.style.width = `calc(25px * ${columns})`
 
-for(let i = 0; i < rows; i++){
+for(let y = 0; y < rows; y++){
   const rowEl = document.createElement('div')
   rowEl.classList.add('row')
 
@@ -16,3 +18,20 @@ for(let i = 0; i < rows; i++){
   }
   mainEl.appendChild(rowEl)
 }
+
+// main window data
+
+const data = []
+
+for(let y = 0; y < rows; y++){
+  const row = []
+
+  for (let x = 0; x < columns; x++) {
+    row.push(0)
+  }
+  data.push(row)
+}
+
+console.log(data);
+
+// start at 11 column
