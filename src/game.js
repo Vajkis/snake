b45697bbc4cc680e5913806fd31478d2849ff7e6
Aurimas <vajkis1@gmail.com>
2022-06-26 11:25:00 +0300

    render window

--- src/game.js
+++ src/game.js
@@ -13,32 +13,32 @@ const app = {
 app.reset.mainData = () => {
   app.data.main = []
 
-  for(let y = 0; y < rows; y++){
+  for(let x = 0; x < columns; x++){
     const row = []
 
-    for (let x = 0; x < columns; x++) {
+    for (let y = 0; y < rows; y++) {
       row.push(0)
     }
-    mainData.push(row)
+    app.data.main.push(row)
   }
 }
 
 app.reset.snakeData = () => {
   app.data.snake = [
     {
-      direction: 'left',
+      direction: 'right',
       x: 11,
       y: 11
     },
 
     {
-      direction: 'left',
+      direction: 'right',
       x: 10,
       y: 11
     },
 
     {
-      direction: 'left',
+      direction: 'right',
       x: 9,
       y: 11
     }
@@ -66,40 +66,40 @@ app.render.snakeData = () => {
   const direction = {}
   const tailBody = 5
 
-  for(let i = 0; i < snakeData.length; i++){
+  for(let i = 0; i < app.data.snake.length; i++){
     const direction = {}
 
-    switch (app.data.snake) {
-      case top:
+    switch (app.data.snake[i].direction) {
+      case 'top':
         direction.head = 1
         direction.tail = -1
         break
 
-      case right:
+      case 'right':
         direction.head = 2
         direction.tail = -2
         break
 
-      case bottom:
+      case 'bottom':
         direction.head = 3
         direction.tail = -3
         break
 
-      case left:
+      case 'left':
         direction.head = 4
         direction.tail = -4
         break
     }
 
-    const x = snakeData[i].x
-    const y = snakeData[i].y
+    const x = app.data.snake[i].x
+    const y = app.data.snake[i].y
 
     if(i === 0){
-      mainData[x][y] = direction.head
-    } else if(i === snakeData.length-1){
-      mainData[x][y] = direction.tail
+      app.data.main[x][y] = direction.head
+    } else if(i === app.data.snake.length-1){
+      app.data.main[x][y] = direction.tail
     } else{
-      mainData[x][y] = tailBody
+      app.data.main[x][y] = tailBody
     }
   }
 }
@@ -112,14 +112,70 @@ app.render.window = () => {
     rowEl.classList.add('row')
 
     for (let x = 0; x < columns; x++) {
-
-      // need some code here
-
       const columnEl = document.createElement('div')
-      columnEl.classList.add('tile')
 
+      switch(app.data.main[x][y]){
+        // empty
+        case 0:
+          columnEl.classList.add('tile')
+          break
+
+        // head
+        case 1:
+          columnEl.classList.add('headTop')
+          break
+
+        case 2:
+          columnEl.classList.add('headRight')
+          break
+
+        case 3:
+          columnEl.classList.add('headBottom')
+          break
+
+        case 4:
+          columnEl.classList.add('headLeft')
+          break
+
+        // tail
+        case -1:
+          columnEl.classList.add('tailTop')
+          break
+
+        case -2:
+          columnEl.classList.add('tailRight')
+          break
+
+        case -3:
+          columnEl.classList.add('tailBottom')
+          break
+
+        case -4:
+          columnEl.classList.add('tailLeft')
+          break
+
+        // tail body
+        case 5:
+          columnEl.classList.add('tail')
+          break
+
+        // food
+        case 6:
+          columnEl.classList.add('food')
+          break
+      }
       rowEl.appendChild(columnEl)
     }
     mainEl.appendChild(rowEl)
   }
 }
+
+app.start = () => {
+  app.reset.mainData()
+  app.reset.snakeData()
+  app.reset.window()
+  app.render.snakeData()
+  app.render.window()
+}
+
+app.start()
